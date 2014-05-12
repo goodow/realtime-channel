69a396d9d7814df2ea7bf9aa7066352d5007779f
larry <i@goodow.com>
2014-05-12 11:40:35 +0800

    Update server launch script

--- run-server.sh
+++ run-server.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Configuration file: https://github.com/goodow/realtime-channel/blob/master/src/main/resources/channel.conf
+## Default configuration: https://github.com/goodow/realtime-channel/blob/master/src/main/resources/channel.conf
 
 
-# Option 1: Build from source and run with Maven 
-bower install # fetch realtime-channel.js from https://github.com/goodow/bower-realtime-channel
-mvn clean install vertx:runMod
+## Option 1: Build from source and run with Maven 
+git clone https://github.com/goodow/realtime-channel.git
+cd realtime-channel
+mvn clean package vertx:runMod
 
 
-# Option 2: Run with pre-installed Vert.x
+## Option 2: Run with pre-installed Vert.x
 # wget https://raw.githubusercontent.com/goodow/realtime-channel/master/src/main/resources/channel.conf
 # vertx runmod com.goodow.realtime~realtime-channel~0.5.5-SNAPSHOT -conf channel.conf
